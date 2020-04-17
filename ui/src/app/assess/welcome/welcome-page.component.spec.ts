e14be817a31fcc88aba7470185ff63ab08303470
Mindful Mike <mike@mindfulsoftware.com>
2020-04-17 10:02:37 -0600

    Initial commit with lint fixes.

--- ui/src/app/assess/welcome/welcome-page.component.spec.ts
+++ ui/src/app/assess/welcome/welcome-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { WelcomePageComponent } from './welcome-page.component'
+import {RouterTestingModule} from '@angular/router/testing'
 
 describe('WelcomePageComponent', () => {
   let component: WelcomePageComponent
@@ -8,6 +9,7 @@ describe('WelcomePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [        RouterTestingModule.withRoutes(['/welcome'])],
       declarations: [ WelcomePageComponent ]
     })
     .compileComponents()
